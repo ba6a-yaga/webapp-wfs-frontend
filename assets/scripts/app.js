4980ef01cba0ba26ed189f3a2f0967fd43ce568e
MIhail Ivantsov <spiritkhl@gmail.com>
2019-09-14 14:37:47 +0300

    mobile version add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,55 +1,27 @@
 
+
 $(document).ready(function() {
-    init_logs( true );
 
-    var windowHref = new URL(window.location.href); 
-    var discount = windowHref.searchParams.get("personal");
-    var originUrl = windowHref.origin+windowHref.pathname;
-
-    if( discount ){
-        Cookies.set('discount', discount )
-    }
-
-
-    tmpl = {
-        login: $.templates("#login"),
-        register: $.templates("#register"),
-        dashboard: $.templates("#dashboard"),
-        userListItem: $.templates("#userListItem"),
-        transactions: $.templates('#transactionItems')
-    }
-
-    getAjaxData('/users/current',{},function(response){
-        if( response == 'Unauthorized'){
-            $('#wrapper').html(tmpl.login.render())
-        }else{
-            window.UserData = response.user;
-            $('#wrapper').html(tmpl.dashboard.render())            
-        }
-
-        $('body')
-            .removeClass('ajaxloader')
-            .addClass('layout-fullwidth')
-    },'GET', 'Get current');
-
-
+    // Logout 
     $('body').on('click', '[data-action="logout"]', function(e){
         e.preventDefault();
         Cookies.remove('Auth');
         $('#wrapper').html(tmpl.login.render())
     })
 
-
+    // Register
     $('body').on('click', '[data-action="register"]', function(e){
         e.preventDefault();
         $('#wrapper').html(tmpl.register.render())
     }) 
 
+    // Login
     $('body').on('click', '[data-action="login"]', function(e){
         e.preventDefault();
         $('#wrapper').html(tmpl.login.render())
     })        
 
+    // Buy tokens start 
     $('body').on('keyup', '[name="tokensAmount"]', function(e){
         e.preventDefault();
         var tokensAmount = $(this).val();
@@ -73,6 +45,8 @@ $(document).ready(function() {
             )
         },'POST','createPayment')
     })
+    // buy tokens end
+
 
     var successPayment = windowHref.searchParams.get("successPayment");
     if(successPayment){
@@ -86,7 +60,7 @@ $(document).ready(function() {
     }
 
 
-
+    // Search users
     $('body').on('submit', 'form[data-action="userSearch"]', function(e){
         e.preventDefault();
         var form = $(this);
@@ -110,6 +84,7 @@ $(document).ready(function() {
 
     })
 
+    // all forms
     $('body').on('submit', 'form[data-form="true"]', function(e){
         e.preventDefault();
 
@@ -181,6 +156,7 @@ $(document).ready(function() {
     })
 
 
+    // delete user
     $('body').on('click', '[data-rmuser]',function(e){
         var userid = $(this).attr('data-rmuser');
         var isDelete = confirm("Вы точно хотите удалить этого пользователя?");
@@ -196,199 +172,3 @@ $(document).ready(function() {
     })    
 
 })
-
-
-function updateUserList(){
-    $('.userListItem tbody').html(tmpl.userListItem.render())
-}
-
-function getUser(){
-    return window.UserData;
-}
-
-function formatDate(dateString){
-    var date = new Date(dateString);
-    return  date.getDate() + "-" + date.getMonth() + "-"+date.getFullYear()+" " + date.getHours() + ":" + date.getMinutes();
-}
-
-function convertBalance(balance){
-    return Number(balance).toLocaleString('ru-RU')
-}
-
-function isMe(phone){
-    return Number(phone) == Number(UserData.phone)
-}
-
-function getDiscount(){
-    return Cookies.get('discount')
-}
-
-$.views.helpers({getUser:getUser,formatDate:formatDate, convertBalance:convertBalance, isMe: isMe, getDiscount: getDiscount });
-
-function getFormData(form){
-    var data = {};
-    form.find('input').each(function(){
-        var name = $(this).attr('name');
-        var value = $(this).val();
-        data[name] = value;
-    })
-    return data;
-}
-
-
-function makePayment(amount,description){
-    var query = {
-        "amount": {
-          "value": amount,
-          "currency": "RUB"
-        },
-        "capture": true,
-        "confirmation": {
-          "type": "redirect",
-          "return_url": "https://www.wfs.com/"
-        },
-        "description": description
-      };
-
-    $.ajax({
-        type : 'POST',
-        url : 'https://payment.yandex.net/api/v3/payments',
-        dataType : 'json',
-        cache : false,
-        data : query,
-        contentType: "application/json",
-        headers: {
-            'Idempotence-Key' : uuidv4(),
-            '635105': 'test_NjM2Mzk28pZuzy3CkilnQvxwac-HWJFDDRykgOkBhUw'
-        },
-        statusCode: {
-            401: function(response) {
-                logs(response)
-                return true;
-            },
-            422: function(response) {
-                logs(response);
-            }
-        },
-        success : function( data ){           
-            
-            logs(data)
-        },
-
-        error : function ( XMLHttpRequest, textStatus, errorThrown ) {
-
-            console.log(errorThrown)
-            if( textStatus == 'parsererror' ){
-                console.log( XMLHttpRequest.responseText );
-            }
-        
-        }
-    
-    });    
-}
-
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
-
-function getAjaxData( path, query, callback, type, stamp ){
-
-
-    if( !isset( type ) )
-        type = 'POST'
-
-    if( !isset( stamp ) )
-        stamp = 'NoStamp';
-
-
-    if(Cookies.get('Auth')){
-        var headers = {
-            'Authorization': "Token "+Cookies.get('Auth')
-        };
-    }else{
-        var headers = '';
-    }
-
-    $.ajax({
-        async: true,
-        type : type,
-        url : urlApi+path,
-        dataType : 'json',
-        cache : false,
-        data : JSON.stringify(query),
-        contentType: "application/json; charset=utf-8",
-        headers: headers,
-        statusCode: {
-            401: function(response) {
-                logs(response)
-                return true;
-            },
-            422: function(response) {
-                logs(response);
-            }
-        },
-        success : function( data ){           
-            if( isset( window.logs ) )
-                logs( '[++AjaxQuery-->' + stamp + '++]', { query : query, result : data } );
-
-            callback( data );
-        },
-
-        error : function ( XMLHttpRequest, textStatus, errorThrown ) {
-
-            callback(errorThrown)
-            if( textStatus == 'parsererror' ){
-                console.log( XMLHttpRequest.responseText );
-            }
-        
-        }
-    
-    });
-
-}
-
-// Логирование
-function init_logs( trigger ){
-
-    if( trigger ){
-        
-        if( isset( window.console ) && typeof arguments != 'undefined' )
-            if( isset( console.log.bind ) )
-                window.logs =  console.log.bind( console );
-            else
-                window.logs = function(){
-                    console.log(arguments)
-                }
-        else
-            window.logs = function(){}
-
-        window.myalert = alert;
-        
-    }else{
-        
-        window.myalert = function(){};
-        window.logs	= function(){}
-    }
-
-}
-
-function isset(e){
-	
-    if( typeof e != 'undefined' && e != '' && typeof e != 'object' )
-        return true;
-    else if( typeof e == 'object' ){
-        for( var key in e ){
-            if( typeof e[key] != 'undefined' )
-                return true;
-        }
-
-        return false;
-    }else
-        return false;
-
-}
-
-
